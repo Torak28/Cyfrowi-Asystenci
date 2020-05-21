3b3b88feabecce94c64b90cc99262fb1d2b8baa4
Jarosław Ciołek-Żelechowski <zelechowski28@gmail.com>
2020-05-21 05:31:54 +0200

    Smth happening here

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import time
+import playsound
+import speech_recognition as sr
+from gtts import gTTS
+
+def speak(text):
+    tts = gTTS(text=text, lang='en')
+    filename = 'voice.mp3'
+    tts.save(filename)
+    playsound.playsound(filename)
+    os.remove(filename)
+
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ''
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+    return said
+
+speak('Hello Jarek')
+text = get_audio()
+
+if 'hello' in text:
+    speak('hello, how are you?')
