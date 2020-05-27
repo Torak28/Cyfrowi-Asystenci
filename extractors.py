deb16e8ee810f0d4aa958ee5777368665cf8766b
Jarosław Ciołek-Żelechowski <zelechowski28@gmail.com>
2020-05-27 15:55:17 +0200

    Better deal

--- extractors.py
+++ extractors.py
@@ -1,41 +1,6 @@
 import requests
 import re
 
-class SpoonacularExtractor():
-    def __init__(self):
-        self.api_key = config.get("CONFIGURATION","yummly_app_id")
-        self.urls = {'list_url': 'https://api.spoonacular.com/recipes/findByIngredients',
-                     'recipe_url': 'https://api.spoonacular.com/recipes/{}/information'}
-
-    def extract_list(self, ingredients=[]):
-        if not ingredients:
-            raise Exception('Ingredients list is empty')
-        
-        ingredients_string = ',+'.join(ingredients)
-        params = {'apiKey': self.api_key,
-                  'ranking': '2',
-                  'ignorePantry': 'false',
-                  'ingredients': ingredients_string}
-
-        url = self.urls['list_url']
-        list_response = requests.request("GET", url=url, params=params)
-        recipe_list = list_response.json()
-        return recipe_list
-
-    def extract_recipe(self, id, nutrition='false'):
-        params = {'apiKey': self.api_key,
-                  'includeNutrition': nutrition}
-
-        url = self.urls['recipe_url'].format(id)
-        recipe_response = requests.request("GET", url=url, params=params)
-        recipe_info = recipe_response.json()
-        return recipe_info
-
-    def remove_html_tags(self, raw_html):
-        cleaner = re.compile('<.*?>')
-        plain_text = re.sub(cleaner, '', raw_html)
-        return plain_text
-
 class YummlyExtractor():
     def __init__(self):
         self.url = 'https://yummly2.p.rapidapi.com/feeds/search'
