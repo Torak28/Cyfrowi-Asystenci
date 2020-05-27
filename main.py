561af41157724de4edb0407bb9ed702910467cac
Jarosław Ciołek-Żelechowski <zelechowski28@gmail.com>
2020-05-27 18:59:36 +0200

    One to rule them all

--- main.py
+++ main.py
@@ -15,7 +15,48 @@ import wolframalpha
 import wikipedia
 import configparser
 import random
-from extractors import YummlyExtractor
+import requests
+import re
+
+
+class YummlyExtractor():
+    def __init__(self):
+        self.url = 'https://yummly2.p.rapidapi.com/feeds/search'
+        self.headers = {'x-rapidapi-host': 'yummly2.p.rapidapi.com',
+                        'x-rapidapi-key': '5f032ce0acmsh944911b8d99d5dcp199441jsn9be35c34e285'}        
+
+    def extract_list(self, ingredients=[]):
+        if not ingredients:
+            raise Exception('Ingredients list is empty')
+        
+        ingredients_string = ',+'.join(ingredients)
+        query = {'q': ingredients_string,
+                 'start': '0',
+                 'maxResult': '10'}
+        url, headers = self.url, self.headers
+
+        response = requests.request("GET", url=url, headers=headers, params=query).json()
+        recipe_list = response['feed']
+        recipe_list = self.parse_list(recipe_list)
+        return recipe_list
+
+    def parse_list(self, recipe_list=[]):
+        if not recipe_list:
+            raise Exception('Recipe list is empty')
+
+        parsed_list = []
+        for recipe in recipe_list:
+            recipe_dict = {}
+            recipe_dict['name'] = recipe['display']['displayName']
+
+            content = recipe['content']
+            recipe_dict['prep_steps'] = content['preparationSteps']
+
+            ingredients_info = content['ingredientLines']
+            ingredient_list = [x['wholeLine'] for x in ingredients_info]
+            recipe_dict['ingredients'] = ingredient_list
+            parsed_list.append(recipe_dict)
+        return parsed_list
 
 
 class Sarah:
@@ -181,6 +222,9 @@ class Sarah:
     def get_question(self, text, phrase):
         return text.split(phrase)[-1][1:]
 
+    def get_ingredients(self, text, phrase):
+        return text.split(phrase)[-1][1:].split(' ')
+
     def ask_wikipedia(self, text):
         print(text)
         result = wikipedia.summary(text, sentences=3)
@@ -189,7 +233,12 @@ class Sarah:
 
     def select_recipe(self, recipe_list):
         recipe_list = [x for x in recipe_list if x['prep_steps']]
-        print(recipe_list)
+        self.speak(f'I found {len(recipe_list[0] + 1)} recipies')
+        for i in len(recipe_list[0]):
+            self.speak(f"{i + 1}: {recipe_list[0][i]['name']}")
+        self.speak('What recipe number You want to cook?')
+        text = self.get_audio()
+        print(text)
         recipe = random.choice(recipe_list)
         return recipe
 
@@ -271,7 +320,8 @@ class Sarah:
                 for phrase in RECIPE_STRS:
                     if phrase in text:
                         self.speak("What ingredients do you have?")
-                        ingredients = self.get_audio().split(' ')
+                        text = self.get_audio()
+                        ingredients = self.get_ingredients(text, 'i have')
                         print(f'Ingredients: {ingredients}')
                         if ingredients:
                             try:
